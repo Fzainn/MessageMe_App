34c218fe44fce5c06ac6586a4e2b40535f6aebf0
fatha <fzainn99@gmail.com>
2024-01-30 21:35:05 +0200

    Add favicon

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,21 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+//= require rails-ujs
+//= require jquery
+//= require activestorage
+//= require turbolinks
+//= require semantic-ui
+//= require_tree .
+
+
+$(document).on('turbolinks:load', function() {
+  $('.ui.dropdown').dropdown();
+})  
+
+
+// document.addEventListener("turbolinks:load", function() {
+//   // Add any specific JavaScript for your dropdown functionality here
+// });
