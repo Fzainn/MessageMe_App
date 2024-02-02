6a00c2b4025aec5e9cde2121704eb19fbcd09b5b
fatha <fzainn99@gmail.com>
2024-02-02 22:03:34 +0200

    add changes

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,6 +3,18 @@ import "@hotwired/turbo-rails"
 import "controllers"
 
 
+/// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
 //= require rails-ujs
 //= require jquery
 //= require activestorage
@@ -10,12 +22,36 @@ import "controllers"
 //= require semantic-ui
 //= require_tree .
 
+scroll_bottom = function() {
+  if ($('#messages').length > 0) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+}
+
+submit_message = function() {
+  $('#message_body').on('keydown', function(e) {
+    if (e.keyCode == 13) {
+      $('button').click();
+      e.target.value = "";
+    };
+  });
+};
+
+
+
+
+
+
 
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
-})  
+
+  $('.message .close').on('click', function() {
+    $(this).closest('.message').transition('fade');
+  });
+  submit_message();
+  scroll_bottom();
+})
 
 
-// document.addEventListener("turbolinks:load", function() {
-//   // Add any specific JavaScript for your dropdown functionality here
-// });
+import "channels"
